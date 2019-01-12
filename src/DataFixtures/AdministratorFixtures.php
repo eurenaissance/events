2fe075c274a61bb01c4e99cefd962f94876a6b35
Rémi Gardien <remi@gardien.biz>
2019-01-12 21:33:31 +0100

    .gitignore

--- src/DataFixtures/AdministratorFixtures.php
+++ src/DataFixtures/AdministratorFixtures.php
@@ -5,7 +5,6 @@ namespace App\DataFixtures;
 use App\Entity\Administrator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Google\GoogleAuthenticatorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdministratorFixtures extends Fixture
@@ -13,12 +12,10 @@ class AdministratorFixtures extends Fixture
     public const DEFAULT_PASSWORD = 'secret!12345';
 
     private $encoder;
-    private $googleAuthenticator;
 
-    public function __construct(UserPasswordEncoderInterface $encoder, GoogleAuthenticatorInterface $googleAuthenticator)
+    public function __construct(UserPasswordEncoderInterface $encoder)
     {
         $this->encoder = $encoder;
-        $this->googleAuthenticator = $googleAuthenticator;
     }
 
     public function load(ObjectManager $manager)
--- vendor/scheb/two-factor-bundle/Tests/TestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Scheb\TwoFactorBundle\Tests;
-
-use PHPUnit\Framework\TestCase as PHPUnitTestCase;
-
-abstract class TestCase extends PHPUnitTestCase
-{
-}
