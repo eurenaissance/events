1f147f470e47a492f977a7950c19eb138404c116
Rémi Gardien <remi@gardien.biz>
2019-02-06 22:00:01 +0100

    Group edition

---
+++ src/Form/Group/GroupType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Form\Group;
+
+use App\Entity\Group;
+use App\Form\DataTransformer\CityToUuidTransformer;
+use App\Util\Slugify;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GroupType extends AbstractType
+{
+    private $slugify;
+    private $cityToUuidTransformer;
+
+    public function __construct(Slugify $slugify, CityToUuidTransformer $cityToUuidTransformer)
+    {
+        $this->slugify = $slugify;
+        $this->cityToUuidTransformer = $cityToUuidTransformer;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'empty_data' => '',
+            ])
+            ->add('description', TextareaType::class, [
+                'empty_data' => '',
+            ])
+            ->add('address', TextType::class, [
+                'required' => false,
+                'empty_data' => '',
+            ])
+            ->add('zipCode', TextType::class, [
+                'mapped' => false,
+            ])
+            ->add('country', CountryType::class, [
+                'mapped' => false,
+            ])
+            ->add('city', HiddenType::class, [
+                'invalid_message' => 'common.city.invalid',
+                'error_bubbling' => false,
+            ])
+            ->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+                $form = $event->getForm();
+                /** @var Group $group */
+                $group = $event->getData();
+
+                $form->get('zipCode')->setData($group->getZipCode());
+                $form->get('country')->setData($group->getCountry());
+            })
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+                /** @var Group $group */
+                $group = $event->getData();
+
+                $this->slugify->createSlug($group);
+            })
+        ;
+
+        $builder->get('city')->addModelTransformer($this->cityToUuidTransformer);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Group::class,
+        ]);
+    }
+}
