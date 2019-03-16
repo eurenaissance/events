4e2b282f76708f68a2a7c6e4dc0d56dd6820f800
Titouan Galopin <galopintitouan@gmail.com>
2019-03-17 00:02:01 +0100

    Implement event search

--- src/Controller/Event/SearchController.php
+++ src/Controller/Event/SearchController.php
@@ -13,6 +13,6 @@ class SearchController extends AbstractController
      */
     public function search(): Response
     {
-        return $this->render('to_implement.html.twig');
+        return $this->render('event/search/search.html.twig');
     }
 }
--- src/Geography/CityRegistry/DatabaseCityRegistry.php
+++ src/Geography/CityRegistry/DatabaseCityRegistry.php
@@ -4,19 +4,13 @@ namespace App\Geography\CityRegistry;
 
 use App\Entity\City;
 use App\Repository\CityRepository;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
 
 class DatabaseCityRegistry implements CityRegistryInterface
 {
-    private const CACHE_TTL = 3600 * 24; // 1 day
-
-    private $cache;
     private $repository;
 
-    public function __construct(CacheInterface $cache, CityRepository $repository)
+    public function __construct(CityRepository $repository)
     {
-        $this->cache = $cache;
         $this->repository = $repository;
     }
 
@@ -25,10 +19,6 @@ class DatabaseCityRegistry implements CityRegistryInterface
         $country = strtoupper($country);
         $zipCode = City::canonicalizeZipCode($zipCode);
 
-        return $this->cache->get('geocode-cities-'.$country.'-'.$zipCode, function (ItemInterface $item) use ($country, $zipCode) {
-            $item->expiresAfter(self::CACHE_TTL);
-
-            return $this->repository->findByZipCode($country, $zipCode);
-        });
+        return $this->repository->findByZipCode($country, $zipCode);
     }
 }
