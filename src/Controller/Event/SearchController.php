4f742864e520ca46726afea9dbf88825909a41f3
Titouan Galopin <galopintitouan@gmail.com>
2019-03-20 23:30:37 +0100

    Finalize MVP of events search

--- src/Controller/Event/SearchController.php
+++ src/Controller/Event/SearchController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Event;
 
-use App\Repository\CityRepository;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +12,10 @@ class SearchController extends AbstractController
     /**
      * @Route("/event/search", name="app_event_search", methods="GET")
      */
-    public function search(EventRepository $repo, CityRepository $cityRepo): Response
+    public function search(EventRepository $repo): Response
     {
-        $events = $repo->search($cityRepo->findOneByUuid('83cd3d14-fc47-4436-b382-cbb0df910a43'), '', 500);
-
         return $this->render('event/search/search.html.twig', [
-            'events' => $events,
+            'events' => $repo->findHomeUpcoming(30),
         ]);
     }
 }
